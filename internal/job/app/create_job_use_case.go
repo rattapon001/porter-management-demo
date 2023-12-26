d2b9c1aac8c4a8c33c15ae607de78c376ab5c380
Rattapon Simsri <rattapon.simsri0@gmail.com>
2023-12-26 16:12:12 +0700

    FEAT : create new job

--- internal/job/app/create_job_use_case.go
+++ internal/job/app/create_job_use_case.go
@@ -1,3 +1,13 @@
 package app
 
-func CreateJob() {}
+import "github.com/rattapon001/porter-management-demo/internal/job/domain"
+
+func (s *JobService) CreateJob(location domain.Location, patient domain.Patient) (*domain.Job, error) {
+	job := domain.CreateNewJob(location, patient)
+	err := s.repo.Save(job)
+	if err != nil {
+		return nil, err
+	}
+	// publish event jobCreated
+	return job, nil
+}
---
+++ internal/job/app/job_service.go
@@ -0,0 +1,17 @@
+package app
+
+import "github.com/rattapon001/porter-management-demo/internal/job/domain"
+
+type JobServicePort interface {
+	CreateJob(location domain.Location, patient domain.Patient) (*domain.Job, error)
+}
+
+type JobService struct {
+	repo domain.JobRepository
+}
+
+func NewJobService(repo domain.JobRepository) *JobService {
+	return &JobService{
+		repo: repo,
+	}
+}
