6f5778aededa2c2af4b6ae4a011a7e8cb65dce2b
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-07 22:48:42 +0700

    FEAT : create func create job

--- api/v1/handlers/job/job_handler.go
+++ api/v1/handlers/job/job_handler.go
@@ -1,3 +1,42 @@
 package job_handler
 
-type JobHandler struct{}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rattapon001/porter-management-demo/internal/job/app"
+	"github.com/rattapon001/porter-management-demo/internal/job/domain"
+)
+
+type JobHandler struct {
+	JobUsecase app.JobServicePort
+}
+
+func NewJobHandler(jobUsecase app.JobServicePort) *JobHandler {
+	return &JobHandler{
+		JobUsecase: jobUsecase,
+	}
+}
+
+func (h *JobHandler) CreateJob(c *gin.Context) {
+	var job domain.Job
+	if err := c.ShouldBindJSON(&job); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	location := domain.Location{
+		From: job.Location.From,
+		To:   job.Location.To,
+	}
+	patient := domain.Patient{
+		Name: job.Patient.Name,
+		HN:   job.Patient.HN,
+	}
+	newJob, err := h.JobUsecase.CreateJob(location, patient)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, newJob)
+
+}
--- api/v1/routers/job/job_router.go
+++ api/v1/routers/job/job_router.go
@@ -2,6 +2,9 @@ package job_router
 
 import (
 	"github.com/gin-gonic/gin"
+	job_handler "github.com/rattapon001/porter-management-demo/api/v1/handlers/job"
+	"github.com/rattapon001/porter-management-demo/internal/job/app"
+	mongoInfra "github.com/rattapon001/porter-management-demo/internal/job/infra/mongo"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,13 +14,13 @@ func InitJobRouter(router *gin.Engine, db *mongo.Client) {
 			"message": "hello job",
 		})
 	})
+	coll := db.Database("porter").Collection("jobs")
+	jobRepository := mongoInfra.NewJobMongoRepository(coll)
+	JobService := app.NewJobService(jobRepository)
+	JobHandler := job_handler.NewJobHandler(JobService)
 
 	jobRouter := router.Group("/jobs")
 	{
-		jobRouter.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "hello job",
-			})
-		})
+		jobRouter.POST("/", JobHandler.CreateJob)
 	}
 }
