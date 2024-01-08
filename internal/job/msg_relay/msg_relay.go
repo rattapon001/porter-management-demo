b6544d0bb120a6d3e40a2f048c96033670f496fe
Rattapon Simsri <rattapon.simsri0@gmail.com>
2024-01-09 02:18:51 +0700

    FEAT : publish event to kafka

--- internal/job/msg_relay/msg_relay.go
+++ internal/job/msg_relay/msg_relay.go
@@ -2,14 +2,23 @@ package msg_relay
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/rattapon001/porter-management-demo/internal/job/domain"
+	job_kafka "github.com/rattapon001/porter-management-demo/internal/job/infra/kafka"
 	job_mongo "github.com/rattapon001/porter-management-demo/internal/job/infra/mongo"
 )
 
+type MyObject struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 func MsgRelay(ctx context.Context) {
+	producer := job_kafka.CreateKafkaProducer()
 	db := job_mongo.MongoDbInit()
 	changeStream := ChangeCapture(db, "porter_management", "jobs", ctx)
 	for changeStream.Next(ctx) {
@@ -24,6 +33,31 @@ func MsgRelay(ctx context.Context) {
 		if changeEvent.FullDocument != nil {
 			// Handle the change event as needed
 			fmt.Printf("Job: %+v\n", *changeEvent.FullDocument)
+			data := changeEvent.FullDocument.Event[len(changeEvent.FullDocument.Event)-1]
+			serializedObj, err := json.Marshal(data)
+
+			topic := "your_topic"
+			message := &kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          serializedObj,
+			}
+			deliveryChan := make(chan kafka.Event)
+			// Produce the message
+			err = producer.Produce(message, deliveryChan)
+			if err != nil {
+				fmt.Printf("Error producing message: %v\n", err)
+				return
+			}
+
+			e := <-deliveryChan
+			m := e.(*kafka.Message)
+			if m.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+			} else {
+				fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+					*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+			}
 		}
 	}
+	defer producer.Close()
 }
